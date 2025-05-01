376fceacd4ba42f367a503a68d63905031df83f3
Renaud Guillard <dev@nore.fr>
2025-05-01 16:48:12 +0200

    v0.5.2 Release

--- scripts/premake5/NoreSources.SSDP.AssemblyInfo.lua
+++ scripts/premake5/NoreSources.SSDP.AssemblyInfo.lua
@@ -1,6 +1,6 @@
 return {
 	AssemblyTitle = "NoreSources SSDP library",
 	AssemblyDescription = ".Net implementation of the SSDP protocol",
-	AssemblyCopyright = "Copyright © Renaud Guillard - 2022",
-	AssemblyVersion = "0.5.1"
+	AssemblyCopyright = "© 2022 Renaud Guillard",
+	AssemblyVersion = "0.5.2"
 }
--- src/NoreSources/SSDP/AssemblyInfo/AssemblyInfo.cs
+++ src/NoreSources/SSDP/AssemblyInfo/AssemblyInfo.cs
@@ -5,7 +5,7 @@
 using System.Reflection;
 
 
-[assembly: AssemblyDescription(".Net implementation of the SSDP protocol")]
-[assembly: AssemblyVersion("0.5.1")]
-[assembly: AssemblyCopyright("Copyright © Renaud Guillard - 2022")]
 [assembly: AssemblyTitle("NoreSources SSDP library")]
+[assembly: AssemblyVersion("0.5.2")]
+[assembly: AssemblyDescription(".Net implementation of the SSDP protocol")]
+[assembly: AssemblyCopyright("© 2022 Renaud Guillard")]
